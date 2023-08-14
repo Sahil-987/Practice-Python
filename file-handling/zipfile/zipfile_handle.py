3e98a2cc7f0e5a0e596f945b13c2a7998d7a40df
Sahil-987 <sahil83765@gmail.com>
2023-08-14 17:25:12 +0530

    zip file handle

--- file-handling/zipfile/zipfile_handle.py
+++ file-handling/zipfile/zipfile_handle.py
@@ -2,8 +2,8 @@ import zipfile
 
 
 # open zip file
-zip_path = "file-handling\zipfile\IAM_Words.zip"
-extract_path = "file-handling\zipfile\zipfile_open"
+# zip_path = "file-handling\zipfile\IAM_Words.zip"
+# extract_path = "file-handling\zipfile\zipfile_open"
 
 # with zipfile.ZipFile(zip_path,'r') as zip_ref:
 #     zip_ref.extractall(extract_path)
