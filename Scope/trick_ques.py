be8e56a92007e6d053a7820366962685973dfd07
Sahil-987 <sahil83765@gmail.com>
2023-10-02 13:59:31 +0530

    Scope in python

---
+++ Scope/trick_ques.py
@@ -0,0 +1,17 @@
+x = 1
+def func():
+    print(x)
+    x = 2
+
+func()
+
+
+"""
+ref: What is Scope in Python?? / b001 - yt
+
+"""
+
+
+# Note : Bhale he print pehle likha, lekin ye compile time error h
+# pehle ye code compile hoga uske baad Python Virtual Machine use Interpret karegi
+
